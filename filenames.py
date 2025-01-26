3590fa61efade436dfc6b5867c421d6114919141
parth-6945 <140963864+parth-6945@users.noreply.github.com>
2025-01-26 14:00:31 +0530

    Delete filenames.py

--- filenames.py
+++
@@ -1,9 +0,0 @@
-import os
-
-directory = r"C:\Users\parth\projects\Compression\Images"
-
-for filename in os.listdir(directory):
-    if "×" in filename:
-        new_name = filename.replace("×", "x")
-        os.rename(os.path.join(directory, filename), os.path.join(directory, new_name))
-        print(f"Renamed: {filename} -> {new_name}")
